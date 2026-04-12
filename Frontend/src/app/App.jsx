ee1b72f1d3b2b819e54bb8d7f09bc4b54f43c769
sanichavhan <chavhansani365@gmail.com>
2026-04-12 21:53:55 +0530

    Add react-router and set up basic routing structure

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import { RouterProvider } from 'react-router'
+import {routes} from './app.routes'
 const App = () => {
   return (
     <div>
-      
+      <RouterProvider router={routes} />
     </div>
   )
 }
---
+++ Frontend/src/app/app.routes.jsx
@@ -0,0 +1,7 @@
+import {createBrowserRouter} from "react-router";
+export const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <h1> Hello World</h1>
+    }
+])
