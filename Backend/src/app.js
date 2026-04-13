59dd4591317199f86e1ed324715356f0c1576de5
sanichavhan <chavhansani365@gmail.com>
2026-04-13 13:00:08 +0530

    fix: update CORS configuration and adjust API base URL for consistency , Use Proxy to forward request to backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,11 +11,11 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use(cookieParser());
-app.use(cors({
-    origin: "http://localhost:5173",
-    methods: [ "GET", "POST", "PUT", "DELETE" ],
-    credentials: true
-}))
+// app.use(cors({
+//     origin: "http://localhost:5173",
+//     methods: [ "GET", "POST", "PUT", "DELETE" ],
+//     credentials: true
+// }))
 
 app.get("/", (_req, res) => {
     res.status(200).json({ message: "Server is running" });
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-    tailwindcss(),
-  ],
+    tailwindcss(),],
+  server :{
+    proxy :{
+      "/api":{
+        target : "http://localhost:3000",
+        changeOrigin : true,
+        secure : false,
+      }
+    }
+  }
   
 })
